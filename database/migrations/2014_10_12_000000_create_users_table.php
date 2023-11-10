eb940c33cfcdaf7fee53bf4237076d82210767d8
ericsaza-InsBc <eric.salado@insbaixcamp.cat>
2023-11-10 19:44:46 +0100

    Afegit els timestamps.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('email')->unique();
             $table->string('password');
-            $table->timestamp('created_at')->nullable();
-            $table->timestamp('last_login')->nullable();
+            $table->timestamps();
         });
     }
 
